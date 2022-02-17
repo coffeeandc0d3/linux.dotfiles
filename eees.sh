3b6a59fe37a5c88e2b49290f03e1484cf001f587
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-02-16 18:21:56 -0600

    self-email & wifi-fix
    used with sendmessage.py to prompt for new msg

---
+++ eees.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "New message:" && cat > /home/pi/Desktop/automation/filemsg
+# Press Ctrl+D to stop recording stuff into file
+cat /home/pi/Desktop/automation/filemsg
---
+++ justinwifi-fix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo rfkill unblock all
+sudo rfkill unblock bluetooth
+sudo rfkill unblock wifi
